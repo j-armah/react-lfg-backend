fafec5eb428c5269956e6a731e996c950d5658ee
februar-y <jarmah241@gmail.com>
2021-02-07 22:11:54 -0500

    serializer update

--- app/serializers/user_game_serializer.rb
+++ app/serializers/user_game_serializer.rb
@@ -1,5 +1,5 @@
 class UserGameSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :game_id, :details, :platform, :level, :server, :image, :user
+  attributes :id, :user_id, :game_id, :details, :platform, :level, :server, :image, :user, :game
 
   
   belongs_to :user
